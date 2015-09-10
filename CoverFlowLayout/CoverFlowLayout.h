0c569edeb8346811e328822a696e7aa105102c91
katiepeterso <katiepeterso@outlook.com>
2015-09-10 12:00:51 -0700

    subclass flow layout

---
+++ CoverFlowLayout/CoverFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.h
+//  CoverFlowLayout
+//
+//  Created by Katherine Peterson on 2015-09-10.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  CoverFlowLayout
+//
+//  Created by Katherine Peterson on 2015-09-10.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
