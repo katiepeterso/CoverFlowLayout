7e2173805629d3201a0320971bd72716775fa994
katiepeterso <katiepeterso@outlook.com>
2015-09-10 15:39:21 -0700

    allow alpha to adjust for each cell based on position

--- CoverFlowLayout/CoverFlowLayout.m
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -7,6 +7,7 @@
 //
 
 #import "CoverFlowLayout.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface CoverFlowLayout ()
 
@@ -22,6 +23,33 @@
     [super prepareLayout];
     self.itemSize = CGSizeMake(self.collectionView.frame.size.width/3, self.collectionView.frame.size.height/2);
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+}
+
+-(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSArray* attributes = [super layoutAttributesForElementsInRect:rect];
+    
+    CGRect visibleRegion;
+    visibleRegion.origin = self.collectionView.contentOffset;
+    visibleRegion.size   = self.collectionView.bounds.size;
+    
+    // Modify the layout attributes as needed here
+    
+    for (UICollectionViewLayoutAttributes *attribute in attributes) {
+        CGFloat visibleCenter = visibleRegion.size.width/2;
+        CGFloat attributeCenter = (attribute.center.x - visibleRegion.origin.x);
+        
+        if (attributeCenter >= visibleCenter) {
+            attribute.alpha = 1-((attributeCenter - visibleCenter)/visibleCenter);
+        }
+        else {
+            attribute.alpha = 1-((visibleCenter - attributeCenter)/visibleCenter);
+        }
+    }
+    return attributes;
+}
+
+-(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
 }
 
 
